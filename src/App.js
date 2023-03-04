825c5761db7f5bf8c658da37dd8d5702e89fa3de
gulfeyzaciftci <gulfeyza333@gmail.com>
2023-03-04 15:16:05 +0100

    all pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import People from "./pages/People";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Nav from "./components/nav/Nav";
+import Footer from "./components/footer/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/PersonDetail.jsx
@@ -0,0 +1,5 @@
+const PersonDetail = () => {
+  return <div></div>;
+};
+
+export default PersonDetail;
